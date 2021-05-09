bceedcf616502fadd164384eb87d492d3afff822
talgat <77709991+TalgatZh@users.noreply.github.com>
2021-05-09 23:33:12 +0600

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,11 @@
+class Person:
+    weight=90
+    heigth=190
+    color_hair='black'
+    gender='male'
+    nation='kyrgyz'
+per1=Person()
+print(per1.nation)
+
+per2=Person()
+print(per2.color_hair)
